7e269fb7150217017d3f94c735c8a959b9f667e1
YipChiSan <57831867+YipChiSan@users.noreply.github.com>
2021-10-20 10:31:41 +1100

    Add some small images under the main image

--- image-gallery/main.js
+++ image-gallery/main.js
@@ -6,8 +6,16 @@ const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+
+let url;
+for (let i = 1; i < 6; i++) {
+    url = "images/pic" + i + ".jpg";
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', url);
+    thumbBar.appendChild(newImage);
+}
+
+
+
 
 /* Wiring up the Darken/Lighten button */
