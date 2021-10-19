4146c3f34e946334428ebca1651b191997d4bb28
YipChiSan <57831867+YipChiSan@users.noreply.github.com>
2021-10-20 10:59:56 +1100

    Add onclick handler on small images

--- image-gallery/main.js
+++ image-gallery/main.js
@@ -6,12 +6,16 @@ const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
 
+function replaceFullImage(e) {
+    displayedImage.setAttribute('src', e.target.src);
+}
 
 let url;
 for (let i = 1; i < 6; i++) {
     url = "images/pic" + i + ".jpg";
     const newImage = document.createElement('img');
     newImage.setAttribute('src', url);
+    newImage.addEventListener('click', replaceFullImage);
     thumbBar.appendChild(newImage);
 }
 
